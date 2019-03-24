a2f1150607c5d07d61b6e524f838a5125abd06b5
aakanksha2 <aakanksha02@hotmail.com>
2019-03-24 15:16:08 -0400

    header

--- scripts/script_sign_in.js
+++ scripts/script_sign_in.js
@@ -1,11 +1,11 @@
 //Sign In Details
 var annDetails;
 
-//Function with alert to see the details of the post
+//Function with alert to see the details of the fill
 function getData() {    
-    annDetails = document.getElementById("details").value;
+    annDetails = document.getElementById("submit").value;
     alert(annDetails);
     
-    localStorage.setItem("details", annDetails);
+    localStorage.setItem("submit", annDetails);
     
 }
